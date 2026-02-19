9631f89dbc355369d6f3c99c64d6118aa7d1353c
alfredo <bt.queso96@gmail.com>
2026-02-18 22:51:33 -0600

    Chore: add helper functions to write and save files

--- hotel_management.py
+++ hotel_management.py
@@ -1,3 +1,20 @@
+import json
+import os
+
+
+def _load(filename: str) -> dict:
+    if not os.path.exists(filename):
+        return {"hotels": {}, "customers": {}, "reservations": {}}
+    with open(filename, "r") as f:
+        return json.load(f)
+
+
+def _save(filename: str, data: dict) -> None:
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
+
+
 class Customer:
     pass
 
